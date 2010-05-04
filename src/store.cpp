a714ca2145c73e27327e27c090ee44bc3631afab
Huy Phan <dachuy@gmail.com>
2010-05-04 16:34:49 +0700

    Change date delimiter from '-' to '_'

--- src/store.cpp
+++ src/store.cpp
@@ -387,8 +387,8 @@ string FileStoreBase::makeBaseFilename(struct tm* creation_time) {
 
   filename << baseFileName;
   if (rollPeriod != ROLL_NEVER) {
-    filename << '-' << creation_time->tm_year + 1900  << '-'
-             << setw(2) << setfill('0') << creation_time->tm_mon + 1 << '-'
+    filename << '_' << creation_time->tm_year + 1900  << '_'
+             << setw(2) << setfill('0') << creation_time->tm_mon + 1 << '_'
              << setw(2) << setfill('0')  << creation_time->tm_mday;
 
   }
@@ -399,8 +399,8 @@ std::string FileStoreBase::makeDateTimeString(struct tm* creation_time) {
   ostringstream datetimeString;
 
   if (rollPeriod != ROLL_NEVER) {
-    datetimeString << creation_time->tm_year + 1900  << '-'
-             << setw(2) << setfill('0') << creation_time->tm_mon + 1 << '-'
+    datetimeString << creation_time->tm_year + 1900  << '_'
+             << setw(2) << setfill('0') << creation_time->tm_mon + 1 << '_'
              << setw(2) << setfill('0')  << creation_time->tm_mday;
 
   }
